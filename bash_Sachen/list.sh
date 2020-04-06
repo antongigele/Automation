15c8750f97f40283c2d6f5d127806e71c5f413ff
Anton Gigele <antongigele@Antons-MacBook-Air-3.local>
2020-04-06 15:08:55 +0200

    newfile

---
+++ bash_Sachen/list.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+for file in $(ls /Users/antongigele/Desktop/ANNEA/Data/Neunkirchen/SCADA_10Min/Neunkirchen/2017/wpneunkirchen_rawdata_20170101_20171231/10min_82888);
+do
+   echo /Users/antongigele/Desktop/ANNEA/Data/Neunkirchen/SCADA_10Min/Neunkirchen/2017/wpneunkirchen_rawdata_20170101_20171231/10min_82888 $file
+done
