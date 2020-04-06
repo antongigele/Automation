2c132f3d052e06a3c4334d8accbe60c0b420ed74
Anton Gigele <antongigele@Antons-MacBook-Air-3.local>
2020-04-06 22:52:32 +0200

    error_removed

--- bash_Sachen/array.sh
+++ bash_Sachen/array.sh
@@ -9,5 +9,5 @@ arr=($files)
 
 for i in {0..11}
 do
-   echo "df$i = pd.read_csv('/Users/antongigele/Desktop/ANNEA/Data/Neunkirchen/SCADA_10Min/Neunkirchen/2017/wpneunkirchen_rawdata_20170101_20171231/10min_82888/${arr[i]}' , sep='')"
+   echo "df$i = pd.read_csv('/Users/antongigele/Desktop/ANNEA/Data/Neunkirchen/SCADA_10Min/Neunkirchen/2017/wpneunkirchen_rawdata_20170101_20171231/10min_82888/${arr[i]}' , sep=';')"
 done
