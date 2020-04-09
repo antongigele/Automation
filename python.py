aec1c489484d975336447fd97cb96e95a329aa8a
Anton Gigele <antongigele@Antons-MacBook-Air-3.local>
2020-04-09 17:48:20 +0200

    justanalyse

--- python.py
+++ python.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
-data = pd.read_csv('/Users/antongigele/Desktop/ANNEA/Data/Neunkirchen/SCADA_10Min/Neunkirchen/2018/wpneunkirchen_rawdata_20180101_20181231/10min_82888/nx82888_rawdata_10m20180101_20180131.csv' , sep=';')
+data = pd.read_csv('/Users/antongigele/Desktop/ANNEA/Data/Neunkirchen/SCADA_10Min/Neunkirchen/2017/Full_data_2017/nx82888_rawdata_10m_2017_full.csv' , sep=';')
 l = len(data.columns)
 print(l)
+print(data.columns)
