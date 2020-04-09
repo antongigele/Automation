c901ac6c0f63bc8a793af180bafc1f0265b98cd2
Anton Gigele <antongigele@Antons-MacBook-Air-3.local>
2020-04-09 14:01:52 +0200

    omit

--- bash_Sachen/array.sh
+++ bash_Sachen/array.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-#print the read-in code for the python script
-cd /Users/antongigele/Desktop/ANNEA/Data/Neunkirchen/SCADA_10Min/Neunkirchen/2017/wpneunkirchen_rawdata_20170101_20171231/10min_82888/
+#print the read-in code for the python script to omit tedious copy-paste
+cd /Users/antongigele/Desktop/ANNEA/Data/Neunkirchen/SCADA_10Min/Neunkirchen/2017/wpneunkirchen_rawdata_20170101_20171231/10min_82889/
 
 files=$( ls * )
 
@@ -10,5 +10,5 @@ arr=($files)
 
 for i in {0..11}
 do
-   echo "df$i = pd.read_csv('/Users/antongigele/Desktop/ANNEA/Data/Neunkirchen/SCADA_10Min/Neunkirchen/2017/wpneunkirchen_rawdata_20170101_20171231/10min_82888/${arr[i]}' , sep=';')"
+   echo "df$i = pd.read_csv('/Users/antongigele/Desktop/ANNEA/Data/Neunkirchen/SCADA_10Min/Neunkirchen/2017/wpneunkirchen_rawdata_20170101_20171231/10min_82889/${arr[i]}' , sep=';')"
 done
