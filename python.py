72466f348fbc8e9d4fa6f2dcaa7c1a173bae4468
Anton Gigele <anton.gigele@aon.at>
2020-05-08 11:42:08 +0200

    unnecessar_files_deleted

--- python.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-data = pd.read_csv('/Users/antongigele/Desktop/ANNEA/Data/Neunkirchen/SCADA_10Min/Neunkirchen/2017/Full_data_2017/nx82888_rawdata_10m_2017_full.csv' , sep=';')
-l = len(data.columns)
-print(l)
-print(data.columns)
