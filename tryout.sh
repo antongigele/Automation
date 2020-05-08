5c4ef3c9c7ff894d94c7daa265cb2e009f92c7c6
antongigele <53263067+antongigele@users.noreply.github.com>
2020-05-08 11:45:24 +0200

    new_tryfile

---
+++ tryout.sh
@@ -0,0 +1,13 @@
+
+LogMessage "Eingabedatei: alles cool"
+LogMessage "Ausgabedatei: das schaffen wir!"
+
+vech="bus"
+
+#print "vehicle is a $vech"
+
+if (( $# != 1 )) ; then
+    LogMessage "Falsche Parameteranzahl"
+    LogMessage "Aufruf: " `basename $0` "gew_kbez"
+    exit 2
+fi
