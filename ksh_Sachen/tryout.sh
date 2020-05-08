eb73187566367dde620d85a3924d121c0ef6c050
Anton Gigele <anton.gigele@aon.at>
2020-05-08 17:17:22 +0200

    newstuff

---
+++ ksh_Sachen/tryout.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/ksh
+
+echo "Positional Parameters"
+echo '$0 = ' $0
+echo '$1 = ' $1
+echo '$2 = ' $2
+echo '$3 = ' $3
+
+
+vech="bus"
+
+if (( $# == 1 )); then
+	echo fuck em all
+else
+	echo wichsgurke	
+fi	
--- tryout.sh
+++ tryout.sh
@@ -4,10 +4,9 @@ LogMessage "Ausgabedatei: das schaffen wir!"
 
 vech="bus"
 
-#print "vehicle is a $vech"
+print "vehicle is a $vech"
 
 if (( $# != 1 )) ; then
     LogMessage "Falsche Parameteranzahl"
     LogMessage "Aufruf: " `basename $0` "gew_kbez"
-    exit 2
 fi
